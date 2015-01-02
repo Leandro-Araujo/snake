d582e8ab40faae9f612bdc6f1b9eba8319d055f0
Leandro Silva Araujo <leandro_sy@hotmail.com>
2015-01-02 20:49:27 -0200

    Snake quase pronta

--- snake.py
+++ snake.py
@@ -1,12 +1,19 @@
 import pygame
 import random
+from pygame.locals import *
+
+def isPointInsideRect(x, y, rect):
+	if (x >= rect.left) and (x <= rect.right) and (y >= rect.top) and (y <= rect.bottom):
+		return True
+	else:
+		return False
 
 pygame.init()
 
 black = [ 0 , 0 , 0]
 white = [255 ,255 ,255]
-foodx = random.randrange(0, 500)
-foody = random.randrange(0, 500)
+foodx = random.randrange(0, 100)
+foody = random.randrange(0, 100)
 snakex = []
 snakey = []
 snakex.append(0)
@@ -22,8 +29,12 @@ done = False
 
 cont = 0
 dir = 'dir'
+b = pygame.draw.rect(screen, black, [foodx*5, foody*5, 20, 20], 0)
 while done == False :
 	screen.fill(white)
+	b = pygame.draw.rect(screen, black, [foodx*5, foody*5, 20, 20], 0)
+
+	
 
 
 	for event in pygame.event.get() : # User did something
@@ -98,7 +109,36 @@ while done == False :
 
 
 	for i in range(len(snakex)):
-		pygame.draw.rect(screen, black, [snakex[i], snakey[i], 20, 20], 0)
+		a = pygame.draw.rect(screen, black, [snakex[i], snakey[i], 20, 20], 0)
 
+	
+	if(isPointInsideRect(a.left, a.top, b) or isPointInsideRect(a.left, a.bottom, b) or isPointInsideRect(a.right, a.top, b) or isPointInsideRect(a.right, a.bottom, b)):
+		if dir == 'left':
+			snakex.append(snakex[len(snakex) - 1] - 5)
+			snakey.append(snakey[len(snakey) - 1])
+			
+			dir = 'left'
+
+		if dir == 'right':
+			snakex.append(snakex[len(snakex) - 1] + 5)
+			snakey.append(snakey[len(snakey) - 1])
+			
+			dir = 'right'
+
+		if dir == 'up':
+			snakex.append(snakex[len(snakex) - 1])
+			snakey.append(snakey[len(snakey) - 1] - 5)
+			
+			dir = 'up'
+
+		if dir == 'down':
+			snakex.append(snakex[len(snakex) - 1])
+			snakey.append(snakey[len(snakey) - 1] + 5)
+			
+			dir = 'down'
+		foodx = random.randrange(0, 100)
+		foody = random.randrange(0, 100)
+		b = pygame.draw.rect(screen, black, [foodx*5, foody*5, 20, 20], 0)
 	pygame.display.flip()
 	clock.tick(20)
+
